e095d59a11893c8b1c260c2dddf21de724d1be79
weijiaxing <weijiaxing765@gmail.com>
2025-05-17 20:10:31 +0800

    1

--- 练习/tailwind_learning/src/app/login1/page.jsx
+++ 练习/tailwind_learning/src/app/login1/page.jsx
@@ -1,8 +1,12 @@
+import React from 'react'
 
 const page = () => {
   return (
-    <div>
-            
+    <div className='m-0 pd-0 bg-red-950'>
+      <aside className='justify-around  text-center font-stretch-100%'>
+        <h1>管理系统</h1>
+      </aside>
+      
     </div>
   )
 }
