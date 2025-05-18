06d075b6e3cf5f57e3a4904e0dcb53fd9879353f
weijiaxing <weijiaxing765@gmail.com>
2025-05-18 11:32:12 +0800

    1

--- 练习/tailwind_learning/src/app/login1/page.jsx
+++ 练习/tailwind_learning/src/app/login1/page.jsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div className='m-0 pd-0 bg-red-950'>
       <aside className='justify-around  text-center font-stretch-100%'>
-        <h1>管理系统</h1>
+        <h1 className='bg-fuchsia-700 hover:bg-fuchsia-900 '>管理系统</h1>
       </aside>
       
     </div>
